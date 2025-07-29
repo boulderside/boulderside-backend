cf2cac9f9c8da0111fd6439bcd52604086c5b14a
=lhj4499 <ucs210212@naver.com>
2025-07-29 17:20:23 +0900

    feat: Security 전역 예외 처리 추가 #15

---
+++ src/main/java/com/example/boulderside/common/security/exception/CustomDeniedHandler.java
@@ -0,0 +1,20 @@
+package com.example.boulderside.common.security.exception;
+
+import java.io.IOException;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CustomDeniedHandler implements AccessDeniedHandler {
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+		AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+	}
+}
