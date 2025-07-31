1995389483617edb603b8372988662e28190112d
이재훈 <potential1205@naver.com>
2025-08-01 00:02:28 +0900

    feature: 사용되지 않는 메서드 제거 #20

--- src/main/java/com/example/boulderside/domain/region/service/RegionService.java
+++ src/main/java/com/example/boulderside/domain/region/service/RegionService.java
@@ -3,7 +3,5 @@ package com.example.boulderside.domain.region.service;
 import com.example.boulderside.domain.region.entity.Region;
 
 public interface RegionService {
-	Region getRegionBtId(Long regionId);
-
 	Region getRegionByProvinceAndCity(String province, String city);
 }
--- src/main/java/com/example/boulderside/domain/region/service/RegionServiceImpl.java
+++ src/main/java/com/example/boulderside/domain/region/service/RegionServiceImpl.java
@@ -15,12 +15,6 @@ public class RegionServiceImpl implements RegionService {
 	private final RegionRepository regionRepository;
 
 	@Override
-	public Region getRegionBtId(Long regionId) {
-		return regionRepository.findById(regionId)
-			.orElseThrow(() -> new DatabaseException(ErrorCode.REGION_NOT_FOUND));
-	}
-
-	@Override
 	public Region getRegionByProvinceAndCity(String province, String city) {
 		return regionRepository.findByProvinceAndCity(province, city)
 			.orElseThrow(() -> new DatabaseException(ErrorCode.REGION_NOT_FOUND));
