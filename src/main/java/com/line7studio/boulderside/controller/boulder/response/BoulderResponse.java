f868539d72c578387fbff6dc249aa2303cb90be7
lhj4499 <ucs210212@naver.com>
2025-08-02 00:09:15 +0900

    chore: likeCnt 컬럼 제거 #28

--- src/main/java/com/line7studio/boulderside/controller/boulder/response/BoulderResponse.java
+++ src/main/java/com/line7studio/boulderside/controller/boulder/response/BoulderResponse.java
@@ -21,7 +21,6 @@ public class BoulderResponse {
 	private String description;
 	private Double latitude;
 	private Double longitude;
-	private Long likeCount;
 	private String province;
 	private String city;
 	private List<String> imageUrlList;
