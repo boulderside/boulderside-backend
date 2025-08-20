265526bfb088ca4a3e15c19c4c5d6b1295183075
이재훈 <potential1205@naver.com>
2025-08-21 00:09:30 +0900

    feat: 날씨 정보 조회 API 파라미터 변경 (좌표 -> 바위 id) #38

--- src/main/java/com/line7studio/boulderside/controller/weather/WeatherController.java
+++ src/main/java/com/line7studio/boulderside/controller/weather/WeatherController.java
@@ -1,18 +1,16 @@
 package com.line7studio.boulderside.controller.weather;
 
-import java.util.List;
-
+import com.line7studio.boulderside.application.weather.WeatherUseCase;
+import com.line7studio.boulderside.application.weather.dto.DailyWeatherInfoDto;
+import com.line7studio.boulderside.common.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.line7studio.boulderside.application.weather.WeatherUseCase;
-import com.line7studio.boulderside.application.weather.dto.DailyWeatherInfoDto;
-import com.line7studio.boulderside.common.response.ApiResponse;
-
-import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 @RestController
 @RequestMapping("/weather")
@@ -22,9 +20,8 @@ public class WeatherController {
 
 	@GetMapping
 	public ResponseEntity<ApiResponse<List<DailyWeatherInfoDto>>> getWeatherInfo(
-		@RequestParam double latitude,
-		@RequestParam double longitude) {
-		List<DailyWeatherInfoDto> dailyWeatherInfoDtoList = weatherUseCase.getWeatherInfo(latitude, longitude);
+		@RequestParam Long boulderId) {
+		List<DailyWeatherInfoDto> dailyWeatherInfoDtoList = weatherUseCase.getWeatherInfo(boulderId);
 		return ResponseEntity.ok(ApiResponse.of(dailyWeatherInfoDtoList));
 	}
 }
