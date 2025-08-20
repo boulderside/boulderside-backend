872e4668f7260d502d6582b73ccf49c59e2a563e
이재훈 <potential1205@naver.com>
2025-08-21 00:10:33 +0900

    feat: 날씨 정보 조회 관련 스케쥴러 설정 및 구현 #38

---
+++ src/main/java/com/line7studio/boulderside/scheduler/weather/WeatherScheduler.java
@@ -0,0 +1,29 @@
+package com.line7studio.boulderside.scheduler.weather;
+
+import com.line7studio.boulderside.application.weather.WeatherUseCase;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@ConditionalOnProperty(name = "scheduler.weather.enabled", havingValue = "true", matchIfMissing = true)
+public class WeatherScheduler {
+    private final WeatherUseCase weatherUseCase;
+
+    /**
+     * 매일 오전 00:00, 오전 06:30 (Asia/Seoul)에 모든 바위 위치의 날씨 데이터를 가져와서 저장
+     */
+    @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")   // 자정
+    @Scheduled(cron = "0 30 6 * * *", zone = "Asia/Seoul")  // 06:30
+    @SchedulerLock(name = "weatherDataFetch", lockAtMostFor = "10m", lockAtLeastFor = "1m")
+    public void fetchWeatherData() {
+        log.info("날씨 정보 업데이트 스케쥴러 실행");
+        weatherUseCase.fetchAndSaveAllWeatherData();
+        log.info("날씨 정보 업데이트 스케쥴러 실행 완료");
+    }
+}
