0d3de5fd2e0b149290a3ce8e83df75b82b972664
이재훈 <potential1205@naver.com>
2025-08-25 17:13:06 +0900

    feat: Post 엔티티 추가 #45

---
+++ src/main/java/com/line7studio/boulderside/domain/aggregate/post/entity/Post.java
@@ -0,0 +1,60 @@
+package com.line7studio.boulderside.domain.aggregate.post.entity;
+
+import com.line7studio.boulderside.domain.BaseEntity;
+import com.line7studio.boulderside.domain.aggregate.post.enums.PostType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "posts")
+public class Post extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    /** 사용자 ID */
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    /** 게시글 제목 */
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    /** 게시글 내용 */
+    @Column(name = "content")
+    private String content;
+
+    /** 게시글 유형 */
+    @Enumerated(EnumType.STRING)
+    @Column(name = "post_type", nullable = false)
+    private PostType postType;
+
+    /** 조회 수 */
+    @Column(name = "view_count")
+    private Integer viewCount;
+
+    /** 동행 날짜 */
+    @Column(name = "meeting_date")
+    private LocalDate meetingDate;
+
+    public void update(String title, String content, PostType postType, LocalDate meetingDate) {
+        this.title = title;
+        this.content = content;
+        this.postType = postType;
+        this.meetingDate = meetingDate;
+    }
+
+    public void incrementViewCount() {
+        this.viewCount = (this.viewCount == null) ? 1 : this.viewCount + 1;
+    }
+}
