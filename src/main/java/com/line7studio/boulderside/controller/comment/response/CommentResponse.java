e595b9e8504736f56943d492f797d80dc7f0e061
이재훈 <potential1205@naver.com>
2025-08-26 14:47:37 +0900

    feat: Comments 레포지토리 구현 (일반, 쿼리) #47

---
+++ src/main/java/com/line7studio/boulderside/controller/comment/response/CommentResponse.java
@@ -0,0 +1,39 @@
+package com.line7studio.boulderside.controller.comment.response;
+
+import com.line7studio.boulderside.common.dto.UserInfo;
+import com.line7studio.boulderside.domain.aggregate.comment.entity.Comment;
+import com.line7studio.boulderside.domain.aggregate.comment.enums.CommentDomainType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentResponse {
+    private Long commentId;
+    private CommentDomainType commentDomainType;
+    private Long domainId;
+    private Boolean isMine;
+    private UserInfo userInfo;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static CommentResponse of(Comment comment, UserInfo userInfo, Boolean isMine) {
+        return CommentResponse.builder()
+                .commentId(comment.getId())
+                .commentDomainType(comment.getCommentDomainType())
+                .domainId(comment.getDomainId())
+                .isMine(isMine)
+                .userInfo(userInfo)
+                .content(comment.getContent())
+                .createdAt(comment.getCreatedAt())
+                .updatedAt(comment.getUpdatedAt())
+                .build();
+    }
+}
