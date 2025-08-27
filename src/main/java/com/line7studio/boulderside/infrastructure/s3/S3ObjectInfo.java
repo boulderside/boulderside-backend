d23159859eccc18f4b77b9b55b3ae86088380e4a
lhj4499 <ucs210212@naver.com>
2025-08-27 15:13:51 +0900

    refactor: Record + Builder 패턴으로 변경 #44

--- src/main/java/com/line7studio/boulderside/infrastructure/s3/S3ObjectInfo.java
+++ src/main/java/com/line7studio/boulderside/infrastructure/s3/S3ObjectInfo.java
@@ -1,10 +1,14 @@
 package com.line7studio.boulderside.infrastructure.s3;
 
+import lombok.Builder;
+
+@Builder
 public record S3ObjectInfo(
-	String url,
-	String key
+	String url
 ) {
-	public static S3ObjectInfo of(String url, String key) {
-		return new S3ObjectInfo(url, key);
+	public static S3ObjectInfo of(String url) {
+		return S3ObjectInfo.builder()
+			.url(url)
+			.build();
 	}
 }
