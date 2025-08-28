943198d38858f3644c53d259a5a1aaf99bde682e
lhj4499 <ucs210212@naver.com>
2025-08-28 16:49:32 +0900

    feat: 회원가입, 계정 연동 API 추가 #44

--- src/main/java/com/line7studio/boulderside/application/user/UserUseCase.java
+++ src/main/java/com/line7studio/boulderside/application/user/UserUseCase.java
@@ -4,17 +4,23 @@ import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.line7studio.boulderside.application.user.dto.response.UserInfoResponse;
+import com.line7studio.boulderside.common.exception.BusinessException;
 import com.line7studio.boulderside.common.exception.ErrorCode;
 import com.line7studio.boulderside.common.exception.ExternalApiException;
 import com.line7studio.boulderside.common.security.provider.AESProvider;
-import com.line7studio.boulderside.controller.user.response.LinkAccountResponse;
+import com.line7studio.boulderside.controller.user.request.PhoneLinkRequest;
+import com.line7studio.boulderside.controller.user.request.SignupRequest;
+import com.line7studio.boulderside.controller.user.response.PhoneLookupResponse;
 import com.line7studio.boulderside.domain.aggregate.user.entity.User;
 import com.line7studio.boulderside.domain.aggregate.user.provider.PhoneAuthProvider;
 import com.line7studio.boulderside.domain.aggregate.user.service.UserService;
 import com.line7studio.boulderside.infrastructure.redis.RedisKeyPrefixType;
 import com.line7studio.boulderside.infrastructure.redis.RedisProvider;
+import com.line7studio.boulderside.infrastructure.s3.S3FolderType;
+import com.line7studio.boulderside.infrastructure.s3.S3Provider;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,6 +33,7 @@ public class UserUseCase {
 	private final AESProvider aesProvider;
 	private final PhoneAuthProvider phoneAuthProvider;
 	private final RedisProvider redisProvider;
+	private final S3Provider s3Provider;
 
 	public UserInfoResponse getUserInfo(Long id) {
 		User user = userService.getUserById(id);
@@ -77,11 +84,53 @@ public class UserUseCase {
 		phoneAuthProvider.sendCertificationCode(phoneNumber, verificationCode);
 	}
 
-	public LinkAccountResponse linkAccountWithPhone(String phoneNumber) {
+	public PhoneLookupResponse lookupUserByPhone(String phoneNumber) {
 		String encodedPhoneNumber = aesProvider.encrypt(phoneNumber);
-		User user = userService.getUserByPhone(encodedPhoneNumber);
 
-		return LinkAccountResponse.from(user);
+		try {
+			User user = userService.getUserByPhone(encodedPhoneNumber);
+			return PhoneLookupResponse.from(user);
+		} catch (BusinessException e) {
+			return PhoneLookupResponse.notExists();
+		}
+	}
+
+	public void linkAccountByPhone(PhoneLinkRequest request) {
+		String encodedPhone = aesProvider.encrypt(request.phoneNumber());
+		userService.updateUserByPhone(encodedPhone, request.email(), request.password());
+	}
+
+	public void signUp(SignupRequest request, MultipartFile file) {
+		String encodedPhone = aesProvider.encrypt(request.phone());
+
+		// 이미지 저장하기
+		String profileImageUrl = null;
+		if (file != null && !file.isEmpty()) {
+			try {
+				profileImageUrl = s3Provider.imageUpload(file, S3FolderType.PROFILE).url();
+			} catch (Exception e) {
+				throw new ExternalApiException(ErrorCode.S3_INVALID_FILE_TYPE);
+			}
+		}
+
+		// 데이터 저장
+		try {
+			userService.createUser(
+				request.nickname(),
+				profileImageUrl,
+				encodedPhone,
+				request.userRole(),
+				request.userSex(),
+				request.userLevel(),
+				request.name(),
+				request.email(),
+				request.password()
+			);
+		} catch (BusinessException e) {
+			s3Provider.deleteImageByUrl(profileImageUrl);
+			throw new ExternalApiException(e.getErrorCode());
+		}
+
 	}
 
 	private String generateAuthCode() {
@@ -89,4 +138,5 @@ public class UserUseCase {
 		int code = 100000 + random.nextInt(900000);
 		return String.valueOf(code);
 	}
+
 }
--- src/main/java/com/line7studio/boulderside/controller/user/UserController.java
+++ src/main/java/com/line7studio/boulderside/controller/user/UserController.java
@@ -1,5 +1,6 @@
 package com.line7studio.boulderside.controller.user;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,16 +8,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.line7studio.boulderside.application.user.UserUseCase;
 import com.line7studio.boulderside.application.user.dto.response.UserInfoResponse;
 import com.line7studio.boulderside.common.response.ApiResponse;
 import com.line7studio.boulderside.common.security.details.CustomUserDetails;
-import com.line7studio.boulderside.controller.user.request.LinkAccountRequest;
 import com.line7studio.boulderside.controller.user.request.PhoneAuthRequest;
+import com.line7studio.boulderside.controller.user.request.PhoneLinkRequest;
+import com.line7studio.boulderside.controller.user.request.PhoneLookupRequest;
+import com.line7studio.boulderside.controller.user.request.SignupRequest;
 import com.line7studio.boulderside.controller.user.request.VerifyCodeRequest;
-import com.line7studio.boulderside.controller.user.response.LinkAccountResponse;
+import com.line7studio.boulderside.controller.user.response.PhoneLookupResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -51,10 +56,28 @@ public class UserController {
 		return ResponseEntity.ok(ApiResponse.of(response));
 	}
 
-	@PostMapping("/phone/link-account")
-	public ResponseEntity<ApiResponse<LinkAccountResponse>> linkPhoneAccount(
-		@RequestBody LinkAccountRequest request) {
-		LinkAccountResponse response = userUseCase.linkAccountWithPhone(request.phoneNumber());
+	@PostMapping("/phone/lookup")
+	public ResponseEntity<ApiResponse<PhoneLookupResponse>> checkPhone(
+		@RequestBody PhoneLookupRequest request) {
+		PhoneLookupResponse response = userUseCase.lookupUserByPhone(request.phoneNumber());
 		return ResponseEntity.ok(ApiResponse.of(response));
 	}
+
+	@PostMapping("/phone/link-account")
+	public ResponseEntity<ApiResponse<Void>> linkPhoneAccount(
+		@RequestBody PhoneLinkRequest request) {
+
+		userUseCase.linkAccountByPhone(request);
+		return ResponseEntity.ok(ApiResponse.success());
+	}
+
+	@PostMapping(value = "/sign-up", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<ApiResponse<Void>> signUp(
+		@RequestPart("data") SignupRequest request,
+		@RequestPart(value = "file", required = false) MultipartFile file
+	) {
+		userUseCase.signUp(request, file);
+		return ResponseEntity.ok(ApiResponse.success());
+	}
+
 }
