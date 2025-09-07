f48ecdaacc671c44e1be5e2596e46671e8958438
이재훈 <potential1205@naver.com>
2025-09-08 00:09:28 +0900

    feat: Route 생성 객체 추가 #59

---
+++ src/main/java/com/line7studio/boulderside/controller/route/request/CreateRouteRequest.java
@@ -0,0 +1,26 @@
+package com.line7studio.boulderside.controller.route.request;
+
+import com.line7studio.boulderside.common.enums.Level;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateRouteRequest {
+
+	@NotNull(message = "연관 바위 ID는 필수입니다")
+	private Long boulderId;
+
+	@NotBlank(message = "루트 이름은 필수입니다")
+	private String name;
+
+	@NotNull(message = "난이도는 필수입니다")
+	private Level routeLevel;
+}
