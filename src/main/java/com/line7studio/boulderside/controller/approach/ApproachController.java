3118bb8b1830d8cd120b4938812dfd1cf20a8568
이재훈 <potential1205@naver.com>
2025-12-09 02:18:31 +0900

    feat: Approach 엔드 포인트 변경 (#78)

--- src/main/java/com/line7studio/boulderside/controller/approach/ApproachController.java
+++ src/main/java/com/line7studio/boulderside/controller/approach/ApproachController.java
@@ -6,20 +6,18 @@ import com.line7studio.boulderside.controller.approach.response.ApproachResponse
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/approaches")
 @RequiredArgsConstructor
 public class ApproachController {
     private final ApproachUseCase approachUseCase;
 
-    @GetMapping
-    public ResponseEntity<ApiResponse<List<ApproachResponse>>> getApproachesByBoulderId(@RequestParam Long boulderId) {
+    @GetMapping("/boulders/{boulderId}/approaches")
+    public ResponseEntity<ApiResponse<List<ApproachResponse>>> getApproachesByBoulderId(@PathVariable Long boulderId) {
         List<ApproachResponse> approacheList = approachUseCase.getApproachesByBoulderId(boulderId);
         return ResponseEntity.ok(ApiResponse.of(approacheList));
     }
