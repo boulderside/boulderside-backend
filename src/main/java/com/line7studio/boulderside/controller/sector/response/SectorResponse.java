47f49de9fc54055adf9084e31f198e3c046db3d7
이재훈 <potential1205@naver.com>
2025-12-08 20:09:33 +0900

    feat: Sector Admin CRUD 구현 (#78)

---
+++ src/main/java/com/line7studio/boulderside/controller/sector/response/SectorResponse.java
@@ -0,0 +1,21 @@
+package com.line7studio.boulderside.controller.sector.response;
+
+import com.line7studio.boulderside.domain.feature.sector.Sector;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class SectorResponse {
+    private Long id;
+    private String sectorName;
+    private String areaCode;
+
+    public static SectorResponse from(Sector sector) {
+        return SectorResponse.builder()
+            .id(sector.getId())
+            .sectorName(sector.getSectorName())
+            .areaCode(sector.getAreaCode())
+            .build();
+    }
+}
