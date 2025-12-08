ab4ed4ede9ee785356837c7c113b0f59cfe2036e
이재훈 <potential1205@naver.com>
2025-12-08 17:21:35 +0900

    refactor: 관리자 인증, 예외 처리 로직 추가 (#78)

--- src/main/java/com/line7studio/boulderside/common/config/SecurityConfig.java
+++ src/main/java/com/line7studio/boulderside/common/config/SecurityConfig.java
@@ -1,7 +1,12 @@
 package com.line7studio.boulderside.common.config;
 
-import java.util.List;
-
+import com.line7studio.boulderside.common.security.constants.SecurityWhitelist;
+import com.line7studio.boulderside.common.security.exception.CustomDeniedHandler;
+import com.line7studio.boulderside.common.security.exception.CustomEntryPoint;
+import com.line7studio.boulderside.common.security.filter.JWTFilter;
+import com.line7studio.boulderside.common.security.filter.LoginFilter;
+import com.line7studio.boulderside.common.security.provider.TokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -15,14 +20,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import com.line7studio.boulderside.common.security.constants.SecurityWhitelist;
-import com.line7studio.boulderside.common.security.exception.CustomDeniedHandler;
-import com.line7studio.boulderside.common.security.exception.CustomEntryPoint;
-import com.line7studio.boulderside.common.security.filter.JWTFilter;
-import com.line7studio.boulderside.common.security.filter.LoginFilter;
-import com.line7studio.boulderside.common.security.provider.TokenProvider;
-
-import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -57,7 +55,7 @@ public class SecurityConfig {
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers(SecurityWhitelist.SWAGGER).permitAll()
 				.requestMatchers(SecurityWhitelist.PUBLIC).permitAll()
-				.requestMatchers("/admin/**").hasRole("ADMIN")
+				.requestMatchers(SecurityWhitelist.ADMIN).hasRole("ADMIN")
 				.anyRequest().authenticated());
 
 		http.exceptionHandling(exception -> exception
@@ -77,7 +75,7 @@ public class SecurityConfig {
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowCredentials(true); // 자격 증명 포함 허용(Authorization 헤더, 쿠기 사용 가능)
-		configuration.setAllowedOrigins(List.of("http://localhost:3000")); //허용할 프론트엔드 도메인
+		configuration.setAllowedOrigins(List.of("http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:63342")); //허용할 프론트엔드 도메인
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")); //허용 메소드
 		configuration.setAllowedHeaders(List.of("*"));
 		configuration.setExposedHeaders(List.of("*"));
